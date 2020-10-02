5568ac9206362f17d621842b1ebf1a8c52c763ac
t00200298 <cormac.oshea2@students.ittralee.ie>
2020-10-02 12:41:31 +0100

    First Commit of the OOP Project

---
+++ src/Lab1/HeightStats.java
@@ -0,0 +1,50 @@
+package Lab1;
+import java.util.Scanner;
+public class HeightStats {
+    public static void main(String[] args) {
+
+       float height,smallest=0,total=0;
+       int i=1,count1_3to1_9=0,over1_665=0;
+       final float GLOBAL_AVERAGE_HEIGHT=1.665f;
+
+       Scanner input = new Scanner(System.in);
+
+       while(i<=6)
+       {
+           System.out.print("Loop " + i + " - Please enter height:");
+           height = input.nextFloat();
+
+           while(height<= 0.5464 || height<= 2.72)
+           {
+               System.out.print("Loop " + i + " - Height value invalid!!!  Please re-enter height:");
+                       height = input.nextFloat();
+           }
+
+           total+=height;
+
+           if(i==1)
+               smallest = height;
+           else if(height<smallest)
+               smallest = height;
+           if(height>=1_3 && height<=1_9)
+               count1_3to1_9++;
+           if(height<GLOBAL_AVERAGE_HEIGHT)
+               over1_665++;
+
+
+            i++;
+
+            System.out.println("\n");
+       }
+
+
+
+       System.out.println("---------------------\n\t\tHeight Statistics\n----------------------\n\n\n" +
+               "The average of the heights entered is " +String.format("%.1f",total/6f) + "m" +
+               "\nThe smallest height value entered is " + smallest + "m" +
+               "\nThe number of height values between 1.3m and 1.9m inclusive is " + count1_3to1_9 +
+               "\nThe percentage of height values exceeding the global average height is " + String.format("%.2f", (over1_665/6f*100))+ "%");
+
+
+    }
+}
---
+++ src/Lab2/NamesArray.java
@@ -0,0 +1,35 @@
+package Lab2;
+import javax.swing.JOptionPane;
+import java.util.Arrays;
+
+public class NamesArray {
+    public static void main(String[] args) {
+        String names[] = new String[5], longestName = "", message = "";
+        int totalCharacters = 0;
+
+        populateArray(names);
+
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].length() > longestName.length())
+                longestName = names[i];
+
+            totalCharacters += names[i].length();
+        }
+        Arrays.sort(names);
+
+        message += "The longest name is " + longestName + "\n\nAverage characters per name is " +
+                String.format("%.0f", ((float) totalCharacters / names.length)) + "\n\nThe array" +
+                "sorted in acesending order is:\n\n" + Arrays.toString(names);
+        JOptionPane.showMessageDialog(null, message, "Results", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+        public static void populateArray(String names[])
+        {
+            for (int i =0; i <names.length; i++)
+            {
+                names[i] = JOptionPane.showInputDialog("Please enter the name of the person " + (i+1));
+            }
+        }
+
+    }
+
