72a1de10b4ccb25db16eff0fef9e7205c2dfab18
t00200298 <cormac.oshea2@students.ittralee.ie>
2020-10-09 20:28:58 +0100

    commit oop

---
+++ src/labsheet5/exercise5/TestBankAccount.java
@@ -0,0 +1,33 @@
+package labsheet5.exercise5;
+
+import labsheet5.exercise4.BankAccount;
+
+import javax.swing.*;
+
+public class TestBankAccount {
+    public static void main(String[] args) {
+        String output="";
+
+        BankAccount account1 = new BankAccount();
+
+        output += "Calling the no-argument BankAccount constructor. The first" +
+                " BankAccount object details are: " + account1.toString();
+
+        BankAccount account2 = new BankAccount("Richy Rich", 2432343, 0.75);
+
+        output += "\n\nCalling the multi-argument BankAccount constructor. The second " +
+                "BankAccount object details are: " + account2.toString();
+
+        BankAccount.setInterestRate(0.5);
+
+        output += "\n\nNow calling the setInterestRate() method to change the interest rate" +
+                " to 0.5\n\nThe first BankAccount details are: " + account1.toString() +
+                "\n\nThe second BankAccount object details are: " + account2.toString();
+
+        JOptionPane.showMessageDialog(null,output,"BankAccount Object Data",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+
+    }
+}
