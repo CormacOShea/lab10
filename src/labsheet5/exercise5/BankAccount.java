fac0f2699a30a3c0850e085ea543f2f8b83bfaee
t00200298 <cormac.oshea2@students.ittralee.ie>
2020-10-09 21:10:19 +0100

    commit oop

--- src/labsheet5/exercise5/BankAccount.java
+++ src/labsheet5/exercise5/BankAccount.java
@@ -1,43 +1,62 @@
 package labsheet5.exercise5;
 
+//BankAccount.java
+/*An instantiable class which defines a bank-account.*/
+
 public class BankAccount {
     private String owner;
     private int number;
     private static double interestRate;
+    private static int count;
+
 
     public BankAccount(){
-       this("Owner Not Available", 0, 0.0);
-
-
+        this("Owner Not Available",0);
     }
 
-    public BankAccount(String owner, int number, double interestRate){
+    public BankAccount(String owner, double interestRate) {
+        incrementCount();
         setOwner(owner);
-        setNumber(number);
+        setNumber(count);
         setInterestRate(interestRate);
     }
 
-    public String getOwner(){
+    private static void incrementCount() {
+        count++;
+    }
+
+    public static int getCount(){
+        return count;
+    }
+
+    public String getOwner() {
         return owner;
     }
-    public void setOwner(String owner){
+
+    public void setOwner(String owner) {
         this.owner = owner;
     }
-    public  int getNumber(){
+
+    public int getNumber() {
         return number;
     }
-    public void setNumber(int number){
+
+    private void setNumber(int number) {
         this.number = number;
     }
-    public static double getInterestRate(){
+
+    public static double getInterestRate() {
         return interestRate;
     }
-    public static void setInterestRate(double interestRate){
+
+    public static void setInterestRate(double interestRate) {
         BankAccount.interestRate = interestRate;
     }
-    public String toString(){
-        return "Owner: " + getOwner() + " Account Number: " + getNumber() +
-                " Interest Rate: " + getInterestRate();
+
+    public String toString() {
+        return "Owner: " + getOwner() + "  Account Number: " + getNumber() +
+                "  Interest Rate: " + getInterestRate();
     }
+
 
 }
